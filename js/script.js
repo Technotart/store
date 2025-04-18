92c4468b8372b76ee8b76d507d85cf3a81052075
Ragheb Yossof <122679640+Technotart@users.noreply.github.com>
2025-04-18 19:54:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+// init Isotope
+var $products = $('.all-products').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $products.isotope({ filter: filterValue });
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
