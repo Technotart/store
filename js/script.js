09bc087ccfeda00dcf1d360b0da9666647f93e29
Ragheb Yossof <122679640+Technotart@users.noreply.github.com>
2025-04-19 15:20:39 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,5 +30,7 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
-
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
 
